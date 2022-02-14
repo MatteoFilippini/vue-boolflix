6f492eade3f60e9df7925c66df4e7f0b02d29f21
MatteoFilippini <matteofilippini8@gmail.com>
2022-02-14 20:04:55 +0100

    created main component

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,7 @@
 <template>
   <div>
     <Header @searchMovie="searchMovie" />
-    <!-- <input type="text" v-model="searchedMovie" placeholder="Cerca qui..." /> -->
-    <!-- <button @click="searchMovie()">Cerca</button> -->
-    <h1>FILM</h1>
-    <div v-for="movie in movies" :key="movie.id">
-      <h4>{{ movie.title }}</h4>
-      <h5>{{ movie.original_title }}</h5>
-      <div>
-        <p
-          v-if="
-            movie.original_language === 'it' || movie.original_language === 'en'
-          "
-        >
-          <img
-            :alt="movie.original_language"
-            :src="require(`./assets/img/${movie.original_language}.png`)"
-          />
-        </p>
-        <p v-else>
-          {{ movie.original_language }}
-        </p>
-      </div>
-      <p>{{ movie.vote_average }}</p>
-      <hr />
-    </div>
-    <h1>SERIE</h1>
-    <div v-for="serie in series" :key="serie.id">
-      <h4>{{ serie.name }}</h4>
-      <h5>{{ serie.original_name }}</h5>
-      <div>
-        <p
-          v-if="
-            serie.original_language === 'it' || serie.original_language === 'en'
-          "
-        >
-          <img
-            :alt="serie.original_language"
-            :src="require(`./assets/img/${serie.original_language}.png`)"
-          />
-        </p>
-        <p v-else>
-          {{ serie.original_language }}
-        </p>
-      </div>
-      <p>{{ serie.vote_average }}</p>
-      <hr />
-    </div>
+    <Main :movies="movies" :series="series" />
   </div>
 </template>
 
@@ -54,11 +9,13 @@
 import axios from "axios";
 
 import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
 
 export default {
   name: "App",
   components: {
     Header,
+    Main,
   },
   data() {
     return {
@@ -93,7 +50,6 @@ export default {
         });
     },
     searchMovie(i) {
-      console.log(i);
       if (!i) return;
       this.query = i;
       this.getMovies();
---
+++ src/components/Main.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <main>
+      <h1>FILM</h1>
+      <div v-for="movie in movies" :key="movie.id">
+        <h4>{{ movie.title }}</h4>
+        <h5>{{ movie.original_title }}</h5>
+        <div>
+          <p
+            v-if="
+              movie.original_language === 'it' ||
+              movie.original_language === 'en'
+            "
+          >
+            <img
+              :alt="movie.original_language"
+              :src="require(`../assets/img/${movie.original_language}.png`)"
+            />
+          </p>
+          <p v-else>
+            <img
+              src="../assets/img/notImage.png"
+              :alt="'{{ movie.original_language }}'"
+            />
+          </p>
+        </div>
+        <p>{{ movie.vote_average }}</p>
+        <hr />
+      </div>
+      <h1>SERIE</h1>
+      <div v-for="serie in series" :key="serie.id">
+        <h4>{{ serie.name }}</h4>
+        <h5>{{ serie.original_name }}</h5>
+        <div>
+          <p
+            v-if="
+              serie.original_language === 'it' ||
+              serie.original_language === 'en'
+            "
+          >
+            <img
+              :alt="serie.original_language"
+              :src="require(`../assets/img/${serie.original_language}.png`)"
+            />
+          </p>
+          <p v-else>
+            {{ serie.original_language }}
+          </p>
+        </div>
+        <p>{{ serie.vote_average }}</p>
+        <hr />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+
+  props: ["movies", "series"],
+};
+</script>
+
+<style>
+</style>
